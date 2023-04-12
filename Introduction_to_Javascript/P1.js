2522a6e5b9a11125e3f992580db65969bf577b3c
unknown <jayachandran9629040900@gmail.com>
2023-04-12 14:14:19 +0530

    Done!

---
+++ Introduction_to_Javascript/P1.js
@@ -0,0 +1,19 @@
+// Problem 2
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+// My name
+let name="Jayachandran Ramu";
+console.log(name);
+
+// My father's name
+name="Ramu Elumali";
+console.log(name);
+
+// My mother's name
+name="Ranjini Ramu";
+console.log(name);
